072997fbcb99a6b791e2c1a9e363caf6a0426573
Tanya <53278501+TatsianaPiatrova@users.noreply.github.com>
2021-06-16 11:46:19 +0300

    Update DateFormatter.kt

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -3,7 +3,14 @@ package subtask1
 class DateFormatter {
 
     // TODO: Complete the following function
-    fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        fun toTextDay(day: String, month: String, year: String): String =
+        try {
+            var date: LocalDateTime = LocalDateTime.of(year.toInt(), month.toInt(), day.toInt(), 0, 0)
+            date.format(DateTimeFormatter.ofPattern("d MMMM, eeee").withLocale(Locale("ru")))
+        }
+        catch (e:DateTimeException)
+        {
+            "Такого дня не существует"
+        }
     }
 }
