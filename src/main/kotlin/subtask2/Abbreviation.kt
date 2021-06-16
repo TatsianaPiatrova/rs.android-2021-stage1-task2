0f5da4a4dcaee53a56bffeca229db1020b3465ad
Tanya <53278501+TatsianaPiatrova@users.noreply.github.com>
2021-06-16 11:46:53 +0300

    Update Abbreviation.kt

--- src/main/kotlin/subtask2/Abbreviation.kt
+++ src/main/kotlin/subtask2/Abbreviation.kt
@@ -2,8 +2,16 @@ package subtask2
 
 class Abbreviation {
 
-    // TODO: Complete the following function
     fun abbreviationFromA(a: String, b: String): String {
-        throw NotImplementedError("Not implemented")
+        var word = a
+        for (liter in word)
+        {
+            if (!b.contains(liter, true)) {
+               word =  word.replace(liter.toString(),"",true )
+            }
+        }
+        if (word.equals(b, true))
+            return "YES"
+        else return "NO"
     }
 }
